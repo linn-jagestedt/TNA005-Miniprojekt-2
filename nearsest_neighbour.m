c1913eac4b8d939dc3d2971cb5921c2f3f98093a
Linn <linn.jagestedt@gmail.com>
2023-04-03 13:23:04 +0200

    Add comments

--- nearsest_neighbour.m
+++ nearsest_neighbour.m
@@ -1,18 +1,21 @@
 TestNumber = testDigits(:,:,9);
 
-TrainData = reshape(trainDigits, [256, 7291]);
+TrainVectors = reshape(trainDigits, [256, 7291]);
 TestVector = reshape(TestNumber, [256, 1]);
 
-Difference = TrainData;
+% Calculate difference between the train images and the test images
+
+Difference = zeros(256, 7291);
 
 for i = 1:7291
-    Difference(:,i) = Difference(:,i) - TestVector;
+    Difference(:,i) = TrainVectors(:,i) - TestVector;
 end
 
-Norm = vecnorm(Difference);
-Min = min(Norm);
-Index = find(Norm==Min);
+% Calculate the Normal of every column in the matrix
+Norm = vecnorm(Difference); 
+
+% Get the index of the smallest value in the list of normals
+Index = find(Norm==min(Norm)); 
 
 ima(TestNumber);
-
 disp(trainAns(Index));
