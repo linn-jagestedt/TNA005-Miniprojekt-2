06f8e17d18b3f4d70c07e89062422efeaf016f66
Linn <32527868+linn-jagestedt@users.noreply.github.com>
2023-04-04 15:58:35 +0200

    Add files via upload

--- centroid.m
+++ centroid.m
@@ -1,24 +1,9 @@
-function[Guess]=centroid(TestVector, TrainVectors, TrainAns)
-
-Mean = zeros(256, 10);
-
-for i = 1:10
-    Total = TrainVectors(:,TrainAns == (i - 1));    
-    Mean(:,i) = mean(Total, 2);
-end
+function[Guess]=centroid(TestVector, MeanVectors)
 
 % Calculate difference between the train vectors and the test vector
-Difference = Mean - TestVector;
-
-for i = 1:10
-    ima(reshape(Mean(:,i), 16,16))
-end 
-
-for i = 1:10
-    ima(reshape(Difference(:,i), 16,16))
-end 
+Difference = MeanVectors - TestVector;
 
 % Calculate the dot product of every column in the matrix (c1 ⋅ c1)
 Norm = vecnorm(Difference); 
 
-Guess = find(min(Norm));
+Guess = find(Norm==min(Norm)) - 1;
--- projekt.m
+++ projekt.m
@@ -2,25 +2,35 @@
 % where every column represents a number
 TrainVectors = reshape(trainDigits, [256, 7291]);
 
-centr = 1; 
-nearest = zeros(1,1);
+% Calculate Mean vectors
+MeanVectors = zeros(256, 10);
+for i = 1:10
+    MeanVectors(:,i) = mean(TrainVectors(:,trainAns == (i - 1)), 2);
+end
 
+centr = 1; 
+nearest = zeros(2,1);
+
+% Test all numbers in test data
 for i = 1:size(testDigits,3)
+
     % Reshape the test number into a vector in R256
     TestVector = reshape(testDigits(:,:,i), [256, 1]);
 
-    for k = 1:size(nearest)
+    % Nearest Neighbour
+    for k = 1:2
         if (testAns(i) == nearest_neighbour(TestVector, TrainVectors, trainAns, k))
             nearest(k) = nearest(k) + 1;
         end
     end
 
-    if (testAns(i) == centroid(TestVector, TrainVectors, trainAns))
+    % Centroid
+    if (testAns(i) == centroid(TestVector, MeanVectors))
         centr = centr + 1;
     end
 end
 
 result = nearest;
-result(size(result) + 1) = centr;
+result(3) = centr;
 
-bar(result);
+bar((result / size(testDigits,3)) * 100);
